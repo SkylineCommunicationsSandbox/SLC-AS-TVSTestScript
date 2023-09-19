805353e30a381b1611b4d8bb100b1a486843e1d2
Thomas Verschuere <thomas.verschuere@skyline.be>
2023-09-19 15:15:55 +0200

    Just a change

--- SLC-AS-TVSTestScript_1/SLC-AS-TVSTestScript_1.cs
+++ SLC-AS-TVSTestScript_1/SLC-AS-TVSTestScript_1.cs
@@ -17,7 +17,7 @@ namespace SLC_AS_TVSTestScript_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			engine.GenerateInformation("Hello World");
+			engine.GenerateInformation("Hello World 123");
 		}
 	}
 }
