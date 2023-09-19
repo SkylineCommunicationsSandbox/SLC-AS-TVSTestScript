c93901913dd72fc1df59ae05c82f48187de8e20e
Thomas Verschuere <thomas.verschuere@skyline.be>
2023-09-19 16:31:41 +0200

    added order

--- SLC-AS-TVSTestScript_1/SLC-AS-TVSTestScript_1.cs
+++ SLC-AS-TVSTestScript_1/SLC-AS-TVSTestScript_1.cs
@@ -4,20 +4,23 @@ namespace SLC_AS_TVSTestScript_1
 	using System.Collections.Generic;
 	using System.Globalization;
 	using System.Text;
+	using Empower.Library.Room0;
 	using Skyline.DataMiner.Automation;
-	
-	/// <summary>
-	/// Represents a DataMiner Automation script.
-	/// </summary>
+
+    /// <summary>
+    /// Represents a DataMiner Automation script.
+    /// </summary>
 	public class Script
 	{
 		/// <summary>
 		/// The script entry point.
 		/// </summary>
-		/// <param name="engine">Link with SLAutomation process.</param>
+		/// <param name = "engine" > Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
 			engine.GenerateInformation("Hello World 123");
-		}
+			var order = OrderFactory.CreateOrder("Bacon Burger");
+			order.Dispatch("Thomas Verschuere");
+        }
 	}
 }
